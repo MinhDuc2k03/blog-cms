914e1132ebb302dd7a4afc03dc6799398459ba5a
MinhDuc2k03 <149854442+MinhDuc2k03@users.noreply.github.com>
2024-05-03 11:52:07 +0700

    fixed image upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      './storage/framework/views/*.php',
+      './resources/views/*.php',
+      './resources/**/*.js',
+      './resources/**/*.vue',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
+  
